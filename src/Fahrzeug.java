176c8cdb2ece6d5b5b8570971873fc8b61afca4f
Dreipunkt <crauc3@gmail.com>
2016-10-06 12:51:09 +0200

    content added

--- src/Fahrzeug.java
+++ src/Fahrzeug.java
@@ -1,5 +1,64 @@
-/**
- * Created by Christian on 06.10.2016.
- */
-public class Fahrzeug {
+public abstract class Fahrzeug {
+    private static int currentID = 0;
+    private String marke;
+    private String modell;
+    private int baujahr;
+    private int grundpreis;
+    private int id;
+
+    // constructor
+
+    public Fahrzeug(String marke, String modell, int baujahr, int grundpreis, int id) {
+        this.marke = marke;
+        this.modell = modell;
+        this.baujahr = baujahr;
+        this.grundpreis = grundpreis;
+        this.id = currentID++;
+    }
+
+    // standard getters and setters
+
+    public String getMarke() {
+        return marke;
+    }
+
+    public String getModell() {
+        return modell;
+    }
+
+    public int getBaujahr() {
+        return baujahr;
+    }
+
+    public int getGrundpreis() {
+        return grundpreis;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setMarke(String marke) {
+        this.marke = marke;
+    }
+
+    public void setModell(String modell) {
+        this.modell = modell;
+    }
+
+    public void setBaujahr(int baujahr) {
+        this.baujahr = baujahr;
+    }
+
+    public void setGrundpreis(int grundpreis) {
+        this.grundpreis = grundpreis;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // methods
+
+
 }
