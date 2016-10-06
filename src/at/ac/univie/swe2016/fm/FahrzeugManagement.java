9fb422c2ee338f2bda338beef24a8e4eda26667f
Dreipunkt <crauc3@gmail.com>
2016-10-06 13:49:27 +0200

    DAO added

--- src/at/ac/univie/swe2016/fm/FahrzeugManagement.java
+++ src/at/ac/univie/swe2016/fm/FahrzeugManagement.java
@@ -1,7 +1,4 @@
 package at.ac.univie.swe2016.fm;
 
-/**
- * Created by Christian on 06.10.2016.
- */
 public class FahrzeugManagement {
 }
---
+++ src/at/ac/univie/swe2016/fm/fahrzeuge/dao/FahrzeugDAO.java
@@ -0,0 +1,4 @@
+package at.ac.univie.swe2016.fm.fahrzeuge.dao;
+
+public interface FahrzeugDAO {
+}
